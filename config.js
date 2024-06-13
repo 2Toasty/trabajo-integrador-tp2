159379cde4a3bbd1b15aeeeab046db07a3d66be9
Facundo Torres <torresfacundo@hotmail.com>
2024-06-13 00:25:03 -0300

    a

---
+++ config.js
@@ -0,0 +1,12 @@
+const PORT = 8080;
+const PERSISTENCE = 'MONGO';
+// revisar esta dirección
+const DBSTR = 'mongodb+srv://admin:admin@clustertp2.vlnfkid.mongodb.net/?retryWrites=true&w=majority&appName=ClusterTP2'
+const BASE = 'tp2integrador'
+
+export default{
+    PORT,
+    PERSISTENCE,
+    DBSTR,
+    BASE
+}
