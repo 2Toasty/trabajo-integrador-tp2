d70bb726e96fabb0691bd105083f00613e9b2112
naranjaFanta <eduardo.tecmet@hotmail.com>
2024-06-19 10:12:00 -0300

    se realiza documentacion de los metodos de la capa de modelo

--- src/models/DAO/heroesMongo.model.js
+++ src/models/DAO/heroesMongo.model.js
@@ -1,8 +1,16 @@
 import MongoConnection from "../MongoConnection.js";
-import { ObjectId } from "mongodb";
+import { Long, ObjectId } from "mongodb";
 class HeroesModelMongoDB {
   constructor() {}
 
+  /**
+   * Obtiene la lista de héroes desde la base de datos MongoDB.
+   *
+   * @async
+   * @function getHeroes
+   * @throws {Error} Si la lista de héroes está vacía.
+   * @returns {Promise<Array>} Una promesa que resuelve a un array de héroes.
+   */
   getHeroes = async () => {
     const Heroes = await MongoConnection.db
       .collection("heroes")
@@ -14,12 +22,32 @@ class HeroesModelMongoDB {
     return Heroes;
   };
 
+  /**
+   * Obtiene un héroe de la base de datos MongoDB por su ID.
+   *
+   * @async
+   * @function getHeroesById
+   * @param {number|string} id - El ID del héroe que se desea obtener. Debe ser un número entero positivo.
+   * @throws {Error} Si el ID es mayor que la cantidad de elementos en la lista de héroes.
+   * @throws {Error} Si el ID es un número negativo.
+   * @throws {Error} Si el ID contiene caracteres no permitidos.
+   * @returns {Promise<Object|null>} Una promesa que resuelve al héroe con el ID especificado, o `null` si no se encuentra.
+   */
   getHeroesById = async (id) => {
+    const array = await MongoConnection.db
+      .collection("heroes")
+      .find({})
+      .toArray();
+    if (id > array.length) {
+      throw new Error(
+        "El número de ID ingresado es mayor a la cantidad de elementos de esta lista."
+      );
+    }
     const simbolosComunes = [
       "!","@","#","$","%","^","&","*","(",")","-",
       "_","=","+","[","]","{","}","\\","|",";","~",
       ":","'",'"',",",".","/","<",">","?","`"
-    ];
+     ];
     if (id < 1) {
       throw new Error("ERROR, ingreso como id un numero negativo.");
     }
@@ -29,11 +57,23 @@ class HeroesModelMongoDB {
       }
     }
     const stringId = parseInt(String(id));
-    const Heroes = await MongoConnection.db.collection("heroes").find({ id: stringId }).toArray();
-    
+    const Heroes = await MongoConnection.db
+      .collection("heroes")
+      .find({ id: stringId })
+      .toArray();
+
     return Heroes[0];
   };
-
+  /**
+   * Agrega un nuevo héroe a la base de datos MongoDB.
+   *
+   * @async
+   * @function postHeroe
+   * @param {Object} prod - El objeto del héroe que se desea agregar.
+   * @param {string} prod.nombre - El nombre del héroe. No puede estar vacío.
+   * @throws {Error} Si el campo nombre está vacío.
+   * @returns {Promise<Object>} Una promesa que resuelve al objeto del héroe que se ha agregado.
+   */
   postHeroe = async (prod) => {
     if (!prod.nombre || prod.nombre.trim() === "") {
       throw new Error("Error: El campo nombre no puede estar vacío.");
@@ -50,63 +90,88 @@ class HeroesModelMongoDB {
     const newHeroe = await MongoConnection.db
       .collection("heroes")
       .insertOne(heroToInsert);
-    return heroToInsert; 
+    return heroToInsert;
   };
-
+  /**
+   * Cambia el nombre de un héroe en la base de datos MongoDB por su ID.
+   *
+   * @async
+   * @function changeHeroNameById
+   * @param {number|string} id - El ID del héroe cuyo nombre se desea cambiar. Debe ser un número entero positivo.
+   * @param {Object} newName - El nuevo nombre del héroe.
+   * @param {string} newName.nombre - El nuevo nombre del héroe. No puede estar vacío.
+   * @throws {Error} Si el ID es mayor que la cantidad de elementos en la lista de héroes.
+   * @throws {Error} Si el ID contiene caracteres no permitidos.
+   * @throws {Error} Si el ID es un número no válido.
+   * @throws {Error} Si el campo del nombre está vacío.
+   * @returns {Promise<Object>} Una promesa que resuelve al resultado de la operación de actualización.
+   */
   changeHeroNameById = async (id, newName) => {
     const array = await MongoConnection.db
-    .collection("heroes")
-    .find({})
-    .toArray();
-    if(id>array.length){
-      throw new Error("El número de ID ingresado es mayor a la cantidad de elementos de esta lista.")
+      .collection("heroes")
+      .find({})
+      .toArray();
+    if (id > array.length) {
+      throw new Error(
+        "El número de ID ingresado es mayor a la cantidad de elementos de esta lista."
+      );
     }
-    const simbolosComunes = [
-      "!","@","#","$","%","^","&","*","(",")","-",
-      "_","=","+","[","]","{","}","\\","|",";","~",
-      ":","'",'"',",",".","/","<",">","?","`"
-    ];
+     const simbolosComunes = [
+       "!","@","#","$","%","^","&","*","(",")","-",
+       "_","=","+","[","]","{","}","\\","|",";","~",
+       ":","'",'"',",",".","/","<",">","?","`"
+      ];
     for (let char of id) {
       if (simbolosComunes.includes(char)) {
         throw new Error("ERROR, el ID contiene caracteres no permitidos.");
       }
     }
-    if(id<=0){
-    throw new Error ("El ID no es válido.")
-  }
-  if(!newName){
-    throw new Error ("El campo del Nombre no puede estar vacio.")
-  }
-    const nuevoId =  parseInt(String(id))
-       const updateName = await MongoConnection.db.collection("heroes").updateOne(
-         { id: nuevoId },
-         { $set: {nombre: newName.nombre}  }
-      );
-      return updateName;
+    if (id <= 0) {
+      throw new Error("El ID no es válido.");
+    }
+    if (!newName) {
+      throw new Error("El campo del Nombre no puede estar vacio.");
+    }
+    const nuevoId = parseInt(String(id));
+    const updateName = await MongoConnection.db
+      .collection("heroes")
+      .updateOne({ id: nuevoId }, { $set: { nombre: newName.nombre } });
+    return updateName;
   };
 
-
+  /**
+   * Elimina un héroe de la base de datos MongoDB por su ID.
+   *
+   * @async
+   * @function deleteHeroe
+   * @param {number|string} id - El ID del héroe que se desea eliminar. Debe ser un número entero positivo.
+   * @throws {Error} Si el ID es mayor que la cantidad de elementos en la lista de héroes.
+   * @throws {Error} Si el héroe con el ID especificado no existe.
+   * @returns {Promise<Object>} Una promesa que resuelve al resultado de la operación de eliminación.
+   */
   deleteHeroe = async (id) => {
+    const array = await MongoConnection.db
+      .collection("users")
+      .find({})
+      .toArray();
+    if (id > array.length) {
+      throw new Error(
+        "El número de ID ingresado es mayor a la cantidad de elementos de esta lista."
+      );
+    }
     const stringId = parseInt(String(id));
-    const hero = await MongoConnection.db.collection("heroes").findOne({ id: stringId });
+    const hero = await MongoConnection.db
+      .collection("heroes")
+      .findOne({ id: stringId });
     if (!hero) {
       throw new Error("Error: el héroe con ese ID no existe.");
     } else {
-      const result = await MongoConnection.db.collection("heroes").deleteOne({ id: stringId });
+      const result = await MongoConnection.db
+        .collection("heroes")
+        .deleteOne({ id: stringId });
       return result;
     }
   };
-
-  deleteAllHeroes = async () => {
-    const result = await MongoConnection.db.collection("heroes").deleteMany({});
-    if (result.deletedCount === 0) {
-      throw new Error("No se encontraron héroes para eliminar.");
-    }
-    return result;
-  };
-
 }
-
-
 
 export default HeroesModelMongoDB;
--- src/models/DAO/usersMongo.model.js
+++ src/models/DAO/usersMongo.model.js
@@ -1,47 +1,91 @@
-import MongoConnection from "../MongoConnection.js"
+import MongoConnection from "../MongoConnection.js";
 import { ObjectId } from "mongodb";
 class UsersModelMongoDB {
-    constructor() {
-
+  constructor() {}
+  /**
+   * Obtiene la lista de usuarios registrados en la aplicación desde la base de datos MongoDB.
+   *
+   * @async
+   * @function getUsers
+   * @throws {Error} Si la lista de usuarios está vacía.
+   * @returns {Promise<Array>} Una promesa que resuelve a un array de usuarios.
+   */
+  getUsers = async () => {
+    const Users = await MongoConnection.db
+      .collection("users")
+      .find({})
+      .toArray();
+    if (Users.length < 1) {
+      throw new Error("Lista vacia. no hay nada para mostrar");
     }
-
-    getUsers = async () => {
-      
-      const Users = await MongoConnection.db.collection("users").find({}).toArray()
-      if(Users.length < 1){
-        throw new Error ("Lista vacia. no hay nada para mostrar");
+    return Users;
+  };
+  /**
+   * Obtiene un usuario registrado en la aplicación desde la base de datos MongoDB por su ID.
+   *
+   * @async
+   * @function getUsersById
+   * @param {number|string} id - El ID del usuario que se desea obtener. Debe ser un número entero positivo.
+   * @throws {Error} Si el ID es mayor que la cantidad de elementos en la lista de usuarios.
+   * @throws {Error} Si el ID es un número negativo.
+   * @throws {Error} Si el ID contiene caracteres no permitidos.
+   * @returns {Promise<Array>} Una promesa que resuelve a un array con el usuario encontrado.
+   */
+  getUsersById = async (id) => {
+    const array = await MongoConnection.db
+      .collection("users")
+      .find({})
+      .toArray();
+    if (id > array.length) {
+      throw new Error(
+        "El número de ID ingresado es mayor a la cantidad de elementos de esta lista."
+      );
+    }
+    const simbolosComunes = [
+      "!","@","#","$","%","^","&","*","(", ")","-","_",
+      "=","+","[","]","{","}","\\","|",";","~",":","'",
+      '"',",",".","/","<",">","?","`",
+    ];
+    if (id < 1) {
+      throw new Error("ERROR, ingreso como id un numero negativo.");
+    }
+    for (let char of id) {
+      if (simbolosComunes.includes(char)) {
+        throw new Error("El ID contiene caracteres no permitidos.");
       }
-      return Users;
-    };
-
-    getUsersById = async (id) => {
-      const simbolosComunes = [
-        "!","@","#","$","%","^","&","*","(",")","-",
-        "_","=","+","[","]","{","}","\\","|",";","~",
-        ":","'",'"',",",".","/","<",">","?","`"
-      ];
-      if (id < 1) {
-        throw new Error("ERROR, ingreso como id un numero negativo.");
-      }
-      for (let char of id) {
-        if (simbolosComunes.includes(char)) {
-          throw new Error("El ID contiene caracteres no permitidos.");
-        }
-      }
-      const stringId = parseInt(String(id));
-      const Users = await MongoConnection.db.collection("users").find({ id: stringId }).toArray();
-      return Users;
+    }
+    const stringId = parseInt(String(id));
+    const Users = await MongoConnection.db
+      .collection("users")
+      .find({ id: stringId })
+      .toArray();
+    return Users;
   };
 
-
-
-    postUser = async (user) => {
-    if (!user.nombre || user.nombre.trim() === "" || !user.mail || user.mail.trim() === "") {
+  /**
+   * Agrega un nuevo usuario a la base de datos MongoDB.
+   *
+   * @async
+   * @function postUser
+   * @param {Object} user - El objeto del usuario que se desea agregar.
+   * @param {string} user.nombre - El nombre del usuario. No puede estar vacío.
+   * @param {string} user.mail - El correo electrónico del usuario. No puede estar vacío y debe ser válido.
+   * @throws {Error} Si el campo nombre o mail está vacío.
+   * @throws {Error} Si el correo electrónico no es válido.
+   * @returns {Promise<Object>} Una promesa que resuelve a un objeto que contiene el estado de la operación y los datos del usuario agregado.
+   */
+  postUser = async (user) => {
+    if (
+      !user.nombre ||
+      user.nombre.trim() === "" ||
+      !user.mail ||
+      user.mail.trim() === ""
+    ) {
       throw new Error("El campo nombre/mail no pueden estar vacíos.");
     }
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailPattern.test(user.mail)) {
-        throw new Error("El email proporcionado no es válido.");
+      throw new Error("El email proporcionado no es válido.");
     }
     const id = new ObjectId();
     const array = await MongoConnection.db
@@ -55,48 +99,89 @@ class UsersModelMongoDB {
     const newUser = await MongoConnection.db
       .collection("users")
       .insertOne(userToInsert);
-      console.log('tipo dato id',typeof userToInsert.id); // typeof newUser.id no te daba nada por que el resultado de pegarle a mongo es un acknowledged y un insertedId 
-    // return newUser; // original
-    return { acknowledged: newUser.acknowledged, nombre: userToInsert.nombre, mail: userToInsert.mail ,id: userToInsert.id}; // creado para que funcione el test
-    }
-    
-    changeUserMailById = async (id, newMail) => {
-      const array = await MongoConnection.db
+    console.log("tipo dato id", typeof userToInsert.id);
+    return {
+      acknowledged: newUser.acknowledged,
+      nombre: userToInsert.nombre,
+      mail: userToInsert.mail,
+      id: userToInsert.id,
+    };
+  };
+  /**
+   * Cambia el correo electrónico de un usuario en la base de datos MongoDB por su ID.
+   *
+   * @async
+   * @function changeUserMailById
+   * @param {number|string} id - El ID del usuario cuyo correo electrónico se desea cambiar. Debe ser un número entero positivo.
+   * @param {Object} newMail - El nuevo correo electrónico del usuario.
+   * @param {string} newMail.mail - El nuevo correo electrónico del usuario. Debe ser válido según el patrón de email.
+   * @throws {Error} Si el ID es mayor que la cantidad de elementos en la lista de usuarios.
+   * @throws {Error} Si el correo electrónico proporcionado no es válido.
+   * @throws {Error} Si el ID no es válido.
+   * @throws {Error} Si el campo del correo electrónico está vacío.
+   * @returns {Promise<Object>} Una promesa que resuelve al resultado de la operación de actualización.
+   */
+  changeUserMailById = async (id, newMail) => {
+    const array = await MongoConnection.db
       .collection("users")
       .find({})
       .toArray();
-      if(id>array.length){
-        throw new Error("El número de ID ingresado es mayor a la cantidad de elementos de esta lista.")
-      }
-      const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (id > array.length) {
+      throw new Error(
+        "El número de ID ingresado es mayor a la cantidad de elementos de esta lista."
+      );
+    }
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailPattern.test(newMail.mail)) {
-        throw new Error("El email proporcionado no es válido.");
+      throw new Error("El email proporcionado no es válido.");
     }
-      if(id<=0){
-      throw new Error ("El ID no es válido.")
+    if (id <= 0) {
+      throw new Error("El ID no es válido.");
     }
-    if(!newMail){
-      throw new Error ("El campo del Mail no puede estar vacio.")
+    if (!newMail) {
+      throw new Error("El campo del Mail no puede estar vacio.");
     }
-      const nuevoId =  parseInt(String(id))
-         const updateMail = await MongoConnection.db.collection("users").updateOne(
-           { id: nuevoId },
-           { $set: {mail: newMail.mail}  }
-        );
-        return updateMail;
-    };
+    const nuevoId = parseInt(String(id));
+    const updateMail = await MongoConnection.db
+      .collection("users")
+      .updateOne({ id: nuevoId }, { $set: { mail: newMail.mail } });
+    return updateMail;
+  };
 
+  /**
+   * Elimina un usuario de la base de datos MongoDB por su ID.
+   *
+   * @async
+   * @function deleteUser
+   * @param {number|string} id - El ID del usuario que se desea eliminar. Debe ser un número entero positivo.
+   * @throws {Error} Si el ID es mayor que la cantidad de elementos en la lista de usuarios.
+   * @throws {Error} Si no se encuentra un usuario con el ID especificado.
+   * @returns {Promise<Object>} Una promesa que resuelve al resultado de la operación de eliminación.
+   */
 
-    deleteUser = async (id) => {
-      const stringId = parseInt(String(id));
-      const user = await MongoConnection.db.collection("users").findOne({ id: stringId });
-      if (!user) {
-        throw new Error("El usuario con ese id no existe.");
-      } else {
-        const result = await MongoConnection.db.collection("users").deleteOne({ id: stringId });
-        return result;
-      }
+  deleteUser = async (id) => {
+    const array = await MongoConnection.db
+      .collection("users")
+      .find({})
+      .toArray();
+    if (id > array.length) {
+      throw new Error(
+        "El número de ID ingresado es mayor a la cantidad de elementos de esta lista."
+      );
     }
-  }
-  
-  export default UsersModelMongoDB;
+    const stringId = parseInt(String(id));
+    const user = await MongoConnection.db
+      .collection("users")
+      .findOne({ id: stringId });
+    if (!user) {
+      throw new Error("El usuario con ese id no existe.");
+    } else {
+      const result = await MongoConnection.db
+        .collection("users")
+        .deleteOne({ id: stringId });
+      return result;
+    }
+  };
+}
+
+export default UsersModelMongoDB;
